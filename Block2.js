eecefcd26da148911dfd1caeba28cac3d1615921
NavsheenMalhotra <70898902+NavsheenMalhotra@users.noreply.github.com>
2020-10-08 12:13:35 +0100

    Add files via upload

---
+++ Block2.js
@@ -0,0 +1,22 @@
+class Block2{
+  constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.4,
+          'friction':1.0,
+          'density': 0.8
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      push();
+      translate(this.body.position.x, this.body.position.y);
+      fill("blue")
+      strokeWeight(2);
+      rectMode(CENTER);
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+}
